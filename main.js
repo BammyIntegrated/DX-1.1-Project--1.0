35ace2bf7f3ef1c9fc0ed6fb0221f41f4a343586
Michael Bamidele Olurotimi <bammydhele@gmail.com>
2022-08-26 06:38:46 +0100

    Modified-Project1

---
+++ main.js
@@ -0,0 +1,14 @@
+var hamburger = document.querySelector('.hamburger__menu')
+var mobilelink = document.querySelector('.mobile__header__nav-linkholder')
+var backdrop = document.querySelector('.backdrop')
+
+
+
+
+
+function Addclass (){
+    hamburger.classList.toggle('display')
+    mobilelink.classList.toggle('displaylinks')
+    backdrop.classList.toggle('displaylinks')
+
+}
